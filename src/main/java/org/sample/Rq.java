df834ec1f9e9f56fb94827edf056dec5a3e55a43
unknown <leeheamin12@gmail.com>
2025-08-01 10:13:39 +0900

    t5 Red

--- src/main/java/org/sample/Rq.java
+++ src/main/java/org/sample/Rq.java
@@ -11,4 +11,18 @@ public class Rq {
         String[] cmdBits = cmd.split("\\?");
         return cmdBits[0];
     }
+
+    public String getParam(String name, String defaultValue) {
+        //등록?이름=홍길동&등록?고향=남원
+        String queryString  = cmd.split("\\?", 2)[1];
+        // 이름=홍길동&등록?공향=남원
+        String[] paramsBits = queryString.split("=", 2);
+
+        String paramName= paramsBits[0];
+        String paramValue = paramsBits[1];
+
+        if (!name.equals(paramName)) return defaultValue;
+
+        return paramValue;
+    }
 }
--- src/test/java/org/sample/RqTest.java
+++ src/test/java/org/sample/RqTest.java
@@ -26,11 +26,29 @@ public class RqTest {
         assertEquals("삭제", actionName);
     }
     @Test
-    @DisplayName("등록이름 = 홍길동 getParam()")
+    @DisplayName("등록?이름=홍길동, getParam()")
     void t3() {
-        Rq rq = new Rq("등록?이름 = 홍길동");
+        Rq rq  = new Rq("등록?이름=홍길동");
         String name = rq.getParam("이름", "");
 
         assertEquals("홍길동", name);
+    }
+
+    @Test
+    @DisplayName("등록?고향=남원, getParam()")
+    void t4() {
+        Rq rq  = new Rq("등록?고향=남원");
+        String name = rq.getParam("고향", "");
+
+        assertEquals("남원", name);
+    }
+
+    @Test
+    @DisplayName("등록?이름=홍길동&등록?고향=남원, getParam()")
+    void t5() {
+        Rq rq  = new Rq("등록?이름=홍길동&등록?고향=남원");
+        String name = rq.getParam("고향", "");
+
+        assertEquals("남원", name);
     }
 }
