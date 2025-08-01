129ad43714b6a59ba2216aa7a285506a74dbc8e6
unknown <leeheamin12@gmail.com>
2025-08-01 09:25:17 +0900

    First Commit

---
+++ src/main/java/org/sample/App.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class App {
+    public void run () {
+        System.out.println("==계산기==");
+    }
+
+    public int plus(int a, int b) {
+
+        return a + b;
+    }
+}
---
+++ src/test/java/org/sample/RqTest.java
@@ -0,0 +1,29 @@
+package org.sample;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RqTest {
+    @Test
+    @DisplayName("수정?id=1")
+    void t1() {
+        Rq rq = new Rq("수정?id=1");
+
+        String actionName= rq.getActionName();
+
+        assertEquals("수정", actionName);
+    }
+
+    @Test
+    @DisplayName("삭제?id=1")
+    void t2() {
+        Rq rq = new Rq("삭제?id=1");
+
+        String actionName= rq.getActionName();
+
+        assertEquals("삭제", actionName);
+    }
+
+}
